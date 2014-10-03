e3f8326e7c79d7e5b9663f980271c70ce8a146f6
Dinindu-Nissanka <dmmnissanka@gmail.com>
2014-10-03 13:36:55 +0530

    Our first commit

---
+++ OOSD_Project_ausia/src/oosd_project_ausia/OOSD_Project_ausia.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package oosd_project_ausia;
+
+/**
+ *
+ * @author Dinindu
+ */
+public class OOSD_Project_ausia {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
