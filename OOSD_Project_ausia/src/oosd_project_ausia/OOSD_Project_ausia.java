058cab22a65ffda6b87888163d1a79b8fa6ccb82
sachithra-pathiraja <sachithra24@gmail.com>
2014-10-03 14:05:43 +0530

    sachithra

--- OOSD_Project_ausia/src/oosd_project_ausia/OOSD_Project_ausia.java
+++ OOSD_Project_ausia/src/oosd_project_ausia/OOSD_Project_ausia.java
@@ -16,6 +16,7 @@ public class OOSD_Project_ausia {
      * @param args the command line arguments
      */
     //paniya ponnayek
+    //asanka ponnayek
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Hello");
