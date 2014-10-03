8a491652ad673777ca27374aa6772922bb6eb0eb
Ruchindra Vithanage <ruchindravithanage@gmail.com>
2014-10-03 13:49:08 +0530

    first commit  Ruchindra

--- OOSD_Project_ausia/src/oosd_project_ausia/OOSD_Project_ausia.java
+++ OOSD_Project_ausia/src/oosd_project_ausia/OOSD_Project_ausia.java
@@ -17,6 +17,7 @@ public class OOSD_Project_ausia {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("Hello");
     }
     
 }
