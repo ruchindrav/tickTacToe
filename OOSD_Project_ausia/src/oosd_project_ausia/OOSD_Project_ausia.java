5781f5dfd8a9cad9226c9adb7a2a5d32ea69bd83
Ruchindra Vithanage <ruchindravithanage@gmail.com>
2014-10-03 14:39:26 +0530

    commit R1

--- OOSD_Project_ausia/src/oosd_project_ausia/OOSD_Project_ausia.java
+++ OOSD_Project_ausia/src/oosd_project_ausia/OOSD_Project_ausia.java
@@ -20,7 +20,7 @@ public class OOSD_Project_ausia {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Hello");
-        
+        System.out.println("Hello            ");
         System.out.println("Asanka Kolukaaraya");
     }
     
